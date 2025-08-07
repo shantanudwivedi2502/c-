6556d599e9812a817ddc2f87d2ff22cf9c54ce0e
shantanudwivedi2502 <shantanudwivediapple2502@gmail.com>
2025-08-07 20:43:16 +0530

    prime

--- first.cpp
+++ first.cpp
@@ -82,7 +82,7 @@ int main() {
     cout << "Reversed number: " << reversed << endl;
     return 0;
 }*/
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 
 int main() {
@@ -96,7 +96,24 @@ int main() {
 
     cout << "Largest element: " << maxVal << endl;
     return 0;
+}*/
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
 }
+
+int main() {
+    int num = 29;
+    cout << (isPrime(num) ? "Prime" : "Not Prime") << endl;
+    return 0;
+}
+
 
 
 
