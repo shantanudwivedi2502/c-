7691ba11fefa97b89c7a773598f81bd1425ee486
shantanudwivedi2502 <shantanudwivediapple2502@gmail.com>
2025-08-08 22:53:45 +0530

    anagram

--- first.cpp
+++ first.cpp
@@ -113,7 +113,7 @@ int main() {
     cout << (isPrime(num) ? "Prime" : "Not Prime") << endl;
     return 0;
 }*/
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 
 int main() {
@@ -132,7 +132,23 @@ int main() {
 
     if (second == INT_MIN) cout << "No second largest element\n";
     else cout << "Second largest element: " << second << endl;
+}*/
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool isAnagram(string s1, string s2) {
+    if (s1.length() != s2.length()) return false;
+    sort(s1.begin(), s1.end());
+    sort(s2.begin(), s2.end());
+    return s1 == s2;
 }
+
+int main() {
+    string s1 = "listen", s2 = "silent";
+    cout << (isAnagram(s1, s2) ? "Anagram" : "Not Anagram");
+}
+
 
 
 
